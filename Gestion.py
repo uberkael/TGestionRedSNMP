702d27dae87c745fe8990354c96567b8ce1610eb
Adriano M <uberkael@users.noreply.github.com>
2015-04-26 10:21:29 +0200

    Eliminando todas las referencias a snimpy para trabajar

--- Gestion.py
+++ Gestion.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 import sys
-from snimpy.manager import Manager as M
-from snimpy.manager import load
 
 ######################
 # Variables globales #
@@ -29,42 +27,45 @@ if (len(sys.argv)>3):
 ###########################
 # Definicion de funciones #
 ###########################
-def setter(m):
+def setter():
 	"Lee el archivo linea a linea y escribe los datos en el dispositivo"
 	f=open(archivo, 'r')
 	for line in f:
 		a=line.split()
 		if (a):
-			print("Valor Anterior de ", a[0], getattr(m, a[0], a[1]))
-			setattr(m, a[0], a[1])
+			# TODO: setOID
+			print("Valor Anterior de", a[0], "TODO: get", a[0])
+			print("TODO: set", a[0], a[1])
 
-def checker(m):
+def checker():
 	"Lee el archivo linea a linea y comprueba los datos en el dispositivo"
 	f=open(archivo, 'r')
 	for line in f:
 		a=line.split()
 		if (a):
+			# TODO: getOID
 			print("Valor buscado", a[0], "=", a[1])
-			if (a[1]==getattr(m, a[0])):
-				print("Correcto")
+			print("TODO: get", a[0], "y comprobacion")
+			# if (a[1]==" get a[0] "):
+				# print("Correcto")
 
 ###################################
 # Cominenza el programa principal #
 ###################################
 if __name__=="__main__":
-	# Carga las mibs
-	load("mibs/RFC1155-SMI.mib")
-	load("mibs/RFC-1212.mib")
-	load("mibs/rfc1213.mib")
-	# Conexion con el servidor
-	m=M(ip, community="public", version=1)
+	# TODO: Carga las mibs
+	print ("Carga las mibs")
+	# TODO: Conexion con el servidor
+	print ("Conexion con el servidor")
 
 	# Solo comprobar
 	if (check):
-		checker(m)
+		checker()
 	# Asignar y comprobar
 	else:
-		setter(m)
-		checker(m)
+		setter()
+		checker()
+	# TODO: Fin->Bucle Idle
+	print("Fin")
 
 
